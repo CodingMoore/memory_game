49322962037350b537386962bf6dab3885e67c97
Randel M <Randel.C.Moore@gmail.com>
2020-11-10 14:00:12 -0800

    add basic HTML visuals
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>

--- src/cards.js
+++ src/cards.js
@@ -12,3 +12,27 @@ const card9 = 5;
 const card10 = 5;
 const card11 = 6;
 const card12 = 6;
+
+function Cards(card1, card2,....) {
+  this.card1 = card1
+}
+
+function Cards() {
+  this.cards[];
+}
+
+function Card() {
+  this.value 
+}
+//psuedo code for the compare
+Card.prototype.compare = function (clicked card1, clicked card2) {
+  if(clicked card1 === clicked card2){
+    this.whatever = true;
+    return true;
+  } else if(clicked card1 != clicked card2){
+    this.whatever = false;
+    return false;
+  }   
+}
+
+let newcards = Cards
